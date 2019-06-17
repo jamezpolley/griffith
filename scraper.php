18ed108cc7186d411312a87968029a9d72d575f5
James Polley <jp@jamezpolley.com>
2019-06-17 16:07:15 +1000

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -35,6 +35,7 @@ $comment_base = "mailto:admin@griffith.nsw.gov.au";
 # Agreed Terms
 $browser = new PGBrowser();
 $page = $browser->get($url_base);
+print $page;
 $form = $page->form();
 $form->set('agreed', 'true');
 $page = $form->submit();
